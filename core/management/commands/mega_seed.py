b140c8e4df0db837bba0b473816019862e59a153
Gerard Lee <gerardklee@hotmail.com>
2022-06-24 10:15:36 -0700

    change structure a bit

--- core/management/commands/mega_seed.py
+++ core/management/commands/mega_seed.py
@@ -5,16 +5,20 @@ from django_seed import Seed
 from users.models import User
 from rooms.models import Room, Photo
 
-
 class Command(BaseCommand):
 
     help = "It seeds the DB with tons of stuff"
 
     def handle(self, *args, **options):
         user_seeder = Seed.seeder()
-        user_seeder.add_entity(User, 20, {"is_staff": False, "is_superuser": False})
+        user_seeder.add_entity(User, 
+            20, 
+            {
+                "is_staff": False,
+                "is_superuser": False,
+            }
+        )
         user_seeder.execute()
-
         users = User.objects.all()
         room_seeder = Seed.seeder()
         room_seeder.add_entity(
